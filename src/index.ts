ba27e49c1a977bb71feac4a6585ba1369afc893a
Dariusz Filipiak <dariusz.filipiak@igt.com>
2020-08-13 23:44:20 +0200

    new version 2.0.64

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@
 import tnpStart from 'tnp';
 
 global['frameworkName'] = 'firedev';
-//#endregion
+
 export const start = async (args, frameworkName, mode) => {
-  //#region @backend
   await (tnpStart as any)(args, frameworkName, mode);
-  //#endregion
 }
 
+//#endregion
 import { Morphi } from 'morphi';
 export import Firedev = Morphi;
