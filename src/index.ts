960f3d861a64d2071dcf6eedafae7b3a46526783
Dariusz Filipiak <dariusz.filipiak@igt.com>
2021-09-09 18:33:48 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@
 global['frameworkName'] = 'firedev';
 import tnpStart from 'tnp';
 
-
-
 export const start = async (args, frameworkName, mode) => {
   await (tnpStart as any)(args, frameworkName, mode);
 }
