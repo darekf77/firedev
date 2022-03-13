0c7d24cfb57865d352d22fb538833ff317087db0
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-03-13 17:58:46 +0100

    update

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+//#region @notForNpm
+//#region @browser
+import { NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'app-firedev',
+template: 'hello from firedev'
+})
+export class FiredevComponent implements OnInit {
+constructor() { }
+
+ngOnInit() { }
+}
+
+@NgModule({
+imports: [],
+exports: [FiredevComponent],
+declarations: [FiredevComponent],
+providers: [],
+})
+export class FiredevModule { }
+//#endregion
+
+//#region @backend
+async function start(port: number)  {
+
+}
+
+export default start;
+
+//#endregion
+
+//#endregion
