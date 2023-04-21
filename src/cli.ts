d63ab8db7cbc4f3aab3756309c535edd487d133c
Dariusz Filipiak <dariusz.filipiak@igt.com>
2023-04-21 23:45:39 +0200

    update

--- src/cli.ts
+++ src/cli.ts
@@ -3,3 +3,4 @@ import { start as tnpStart } from 'tnp/cli';
 export const start = async (args, frameworkName, mode) => {
   await (tnpStart as any)(args, frameworkName, mode);
 }
+// DONT CHANGE THIS FILE!
