9a632b9851c3c9abb830caac47fe3e5468de1c8c
Dariusz Filipiak <dariusz.filipiak@igt.com>
2023-05-30 20:58:07 +0200

    update

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,13 @@
 import { start as tnpStart } from 'tnp/cli';
+import { start as tnpStartLocal } from 'tnp';
+
+
+let startFn = tnpStart;
+//#region @notForNpm
+startFn = tnpStartLocal;
+//#endregion
 
 export const start = async (args, frameworkName, mode) => {
-  await (tnpStart as any)(args, frameworkName, mode);
+  await (startFn as any)(args, frameworkName, mode);
 }
 // DONT CHANGE THIS FILE!
